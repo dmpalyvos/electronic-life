b9a7dba8536280fc809c1c12867311aeea28580e
dmpalyvos <dm.palyvos@gmail.com>
2016-01-03 14:10:40 +0200

    Added World object.

--- life.js
+++ life.js
@@ -45,6 +45,10 @@ Grid.prototype.set = function(vector, value) {
 };
 
 
+// Wall Class
+function Wall() {
+}
+
 // Critter Class
 var directions = {
     'n': new Vector(0, -1),
@@ -76,8 +80,57 @@ BouncingCritter.prototype.act = function(view) {
              direction: this.direction };
 };
 
+
+// World Class
+function elementFromChar(legend, ch) {
+    if (ch === ' ') {
+        return null;
+    }
+    var element = new legend[ch]();
+    element.originChar = ch;
+    return element;
+}
+
+function charFromElement(element) {
+    if (element === null) {
+        return ' ';
+    }
+    else {
+        return element.originChar;
+    }
+}
+
+function World(map, legend) {
+    var grid = new Grid(map[0].length, map.length);
+    this.grid = grid;
+    this.legend = legend;
+
+    map.forEach(function(line, y) {
+        for (var x = 0; x < line.length; x++) {
+            grid.set(new Vector(x, y), elementFromChar(legend, line[x]));
+        }
+    });
+}
+
+World.prototype.toString = function() {
+    var output = '';
+    for (var y = 0; y < this.grid.height; y++) {
+        for (var x = 0; x < this.grid.width; x++) {
+            var element = this.grid.get(new Vector(x, y));
+            output += charFromElement(element);
+        }
+        output += '<br>';
+    }
+    return output;
+};
+
+
+var world = new World(plan, { '#': Wall, 'o': BouncingCritter } );
+draw(world.toString());
+console.log(world.toString());
+
 // Presentation Functions
 function draw(str) {
-    document.getElementById('world').textContent = str;
+    document.getElementById('world').innerHTML = '<p>' + str + '</p>';
 }
 
